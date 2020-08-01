aee3f60f26e4b14289e754065498418df995cf98
Sebastian <SebastianGillespie99@gmail.com>
2020-08-01 12:43:11 -0400

    finished day 3

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const tagsRouter = express.Router();
-const { getAllTags } = require('../db');
+const { getAllTags, getPostsByTagName } = require('../db');
 
 tagsRouter.use((req, res, next) => {
     console.log("a request is being made to /tags")
@@ -15,5 +15,26 @@ tagsRouter.get('/', async (req, res) => {
         tags
     })
 })
+
+
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+    // read the tagname from the params
+    const   tagname  = req.params.tagName;
+    try {
+      // use our method to get posts by tag name from the db
+        
+        
+        const originalTags = await getPostsByTagName(tagname)
+        
+        res.send({post: originalTags})
+
+
+      // send out an object to the client { posts: // the posts }
+    } catch ({ name, message }) {
+      // forward the name and message to the error handler
+      next({name, message});
+    }
+  });
 
 module.exports = tagsRouter;
--- api/users.js
+++ api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const usersRouter = express.Router();
-const { getAllUsers, getUserByUsername, createUser } = require('../db');
+const { getAllUsers, getUserByUsername, createUser, } = require('../db');
 const jwt = require('jsonwebtoken')
 
 
